feff121fde763d9e91def58064eacc2b5fe9af0a
Gaurav Sharma <77984221+psykick-21@users.noreply.github.com>
2025-04-09 21:42:36 +0530

    Update state.py

--- deep_research/state.py
+++ deep_research/state.py
@@ -1,9 +1,25 @@
 from typing import Annotated, List
 from langchain_core.messages import BaseMessage
-from langchain_core.pydantic_v1 import BaseModel
 import operator
+from typing import TypedDict
+from .struct import Section, Feedback, Query, SearchResult
 
-class AgentState(BaseModel):
+class AgentState(TypedDict):
     topic: str
     outline: str
     messages: Annotated[List[BaseMessage], operator.add]
+    report_structure: str
+    sections: List[Section]
+    final_section_content: Annotated[List[str], operator.add]
+    final_report_content: str
+
+class ResearchState(TypedDict):
+    section: Section
+    knowledge: str
+    reflection_feedback: Feedback
+    generated_queries: List[Query]
+    searched_queries: Annotated[List[Query], operator.add]
+    search_results: Annotated[List[SearchResult], operator.add]
+    accumulated_content: str
+    reflection_count: int
+    final_section_content: List[str]
