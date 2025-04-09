e574449ffdda74cb6dd1e6e9933f76790b8f6ed0
Gaurav Sharma <77984221+psykick-21@users.noreply.github.com>
2025-04-09 21:42:22 +0530

    Update configuration.py

--- deep_research/configuration.py
+++ deep_research/configuration.py
@@ -1,12 +1,32 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 from langchain_core.runnables import RunnableConfig
+import os
+from typing import Any
 
-@dataclass
+@dataclass(kw_only=True)
 class Configuration:
+
+    provider: str = "openai"
+    model: str = "gpt-4o-mini"
+    temperature: float = 0.5
+    max_queries: int = 3
+    search_depth: int = 2
+    num_reflections: int = 2
     
     @classmethod
     def from_runnable_config(
         cls,
         config: RunnableConfig
-    ):
-        pass
+    ) -> "Configuration":
+        
+        configurable = (
+            config["configurable"] if config and "configurable" in config else {}
+        )
+
+        values: dict[str, Any] = {
+            f.name: os.environ.get(f.name.upper(), configurable.get(f.name, f.default))
+            for f in fields(cls)
+            if f.init
+        }
+
+        return cls(**values)
