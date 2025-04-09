1f5901e035649fdbd6c7b329e5c02cecbff845c5
Gaurav Sharma <77984221+psykick-21@users.noreply.github.com>
2025-04-09 21:42:40 +0530

    Create struct.py

---
+++ deep_research/struct.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+from typing import List, Union, Annotated
+import operator
+
+
+class Section(BaseModel):
+    section_name: str = Field(..., description="The name of this section of the report without its number")
+    sub_sections: List[str] = Field(..., description="Comprehensive descriptions of sub-sections, each combining the sub-section title and its bullet points into a fluid, natural-language description")
+
+class Sections(BaseModel):
+    sections: List[Section] = Field(..., description="A list of sections")
+
+class Query(BaseModel):
+    query: str = Field(..., description="A search query")
+
+class Queries(BaseModel):
+    queries: List[Query] = Field(..., description="A list of search queries")
+
+class SearchResult(BaseModel):
+    query: Query = Field(..., description="The search query that was used to retrieve the raw content")
+    raw_content: list[str] = Field(..., description="The raw content retrieved from the search")
+
+class Feedback(BaseModel):
+    feedback: Union[str, bool] = Field(..., description="Feedback on the report structure. If the content is good for the section, return True (boolean), otherwise return a string of feedback on what is missing or incorrect.")
+
+class SectionOutput(BaseModel):
+    final_section_content: List[str] = Field(..., description="The final section content")
