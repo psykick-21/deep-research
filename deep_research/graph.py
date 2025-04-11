2f92c3a9bbe82bde557ebb534fe46d40da343db0
Gaurav Sharma <77984221+psykick-21@users.noreply.github.com>
2025-04-11 11:25:51 +0530

    Update graph.py

--- deep_research/graph.py
+++ deep_research/graph.py
@@ -6,13 +6,14 @@ from .nodes import (
     report_structure_planner_node,
     human_feedback_node,
     section_formatter_node,
-    final_report_writer_node,
     section_knowledge_node,
     query_generator_node,
     tavily_search_node,
     result_accumulator_node,
     reflection_feedback_node,
-    final_section_formatter_node
+    final_section_formatter_node,
+    queue_next_section_node,
+    finalizer_node
 )
 
 
@@ -45,12 +46,14 @@ builder = StateGraph(AgentState)
 builder.add_node("report_structure_planner", report_structure_planner_node)
 builder.add_node("human_feedback", human_feedback_node)
 builder.add_node("section_formatter", section_formatter_node)
+builder.add_node("queue_next_section", queue_next_section_node)
 builder.add_node("research_agent", research_builder.compile())
-builder.add_node("final_report_writer", final_report_writer_node)
+builder.add_node("finalizer", finalizer_node)
 
 builder.set_entry_point("report_structure_planner")
 builder.add_edge("report_structure_planner", "human_feedback")
-builder.add_edge("research_agent", "final_report_writer")
-builder.add_edge("final_report_writer", END)
+builder.add_edge("section_formatter", "queue_next_section")
+builder.add_edge("research_agent", "queue_next_section")
+builder.add_edge("finalizer", END)
 
 agent_graph = builder.compile(checkpointer=memory_saver)
