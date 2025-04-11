4124820e07ad59d9ac636b1a49cfb635c5b050bb
Gaurav Sharma <77984221+psykick-21@users.noreply.github.com>
2025-04-11 11:26:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 from deep_research.graph import agent_graph
 from IPython.display import Image, display
 import uuid
+import os
 
 def main():
-    TOPIC = "Support Vector Machines"
-    OUTLINE = "I am studying machine learning and I want to understand Support Vector Machines."
+    # TOPIC = "Human Psychology"
+    # OUTLINE = "To understand human behaviour about why they always take actions that are selfish and in their best interest, leaving the rest of the world to suffer, including the ones who had supported and loved them all along."
+    TOPIC = "LLM Benchmarking"
+    OUTLINE = "Since there are so many LLMs out there, I want to understand how they compare to each other in terms of their capabilities, performance, and effectiveness. I have seen so many benchmark scores, but I want to understand the most important ones, what they measure, and what they mean."
     
     thread = {
         "configurable": {
             "thread_id": str(uuid.uuid4()),
-            "max_queries": 2,
-            "search_depth": 2,
-            "num_reflections": 2,
-            "provider": "ollama",
-            "model": "gemma3:4b",
-            "temperature": 0.3
+            "max_queries": 3,
+            "search_depth": 3,
+            "num_reflections": 3,
+            "temperature": 0.7
         }
     }
+
+    os.makedirs("logs", exist_ok=True)
+    os.makedirs("report", exist_ok=True)
+
     for event in agent_graph.stream(
         {"topic": TOPIC, "outline": OUTLINE},
         config=thread,
     ):
-        with open("agent_logs.txt", "a") as f:
+        with open("logs/agent_logs.txt", "a") as f:
             f.write(str(event))
             f.write("\n\n\n")
 
@@ -30,22 +35,21 @@ def main():
             print(event["report_structure_planner"]["messages"][-1].content)
             print("\n", "="*100, "\n")
         elif "section_formatter" in event:
-            print("<<< SECTION FORMATTING >>>")
-            print(event["section_formatter"])
-            print("\n", "="*100, "\n")
+            pass
         elif "research_agent" in event:
             # check output of research_agent
-            print("<<< RESEARCH AGENT >>>")
-            print(event["research_agent"])
-            print("\n", "="*100, "\n")
-        elif "final_report_writer" in event:
-            # check output of final_report_writer
-            print("<<< FINAL REPORT WRITER >>>")
-            print(event["final_report_writer"])
-            print("\n", "="*100, "\n")
-        else:
+            pass
+        elif "human_feedback" in event:
             print("<<< HUMAN FEEDBACK >>>")
             print(event["human_feedback"]["messages"][-1].content)
+            print("\n", "="*100, "\n")
+        elif "queue_next_section" in event:
+            pass
+        elif "finalizer" in event:
+            print("Final report complete.")
+        else:
+            print("<<< UNKNOWN EVENT >>>")
+            print(event)
             print("\n", "="*100, "\n")
     
 
